fe07fa2edad8349659b4a9ee2419b4d447c0255c
adrianopb <adrianopbraga1@gmail.com>
2020-10-26 22:56:27 -0300

    commit inicial

---
+++ calculator.js
@@ -0,0 +1,22 @@
+const sum = (x,y) => {
+    return x + y
+}
+
+const sub = (x,y) => {
+    return x - y
+}
+
+const mult = (x,y) => {
+    return x * y
+}
+
+const div = (x,y) => {
+    return x / y
+}
+
+module.exports = {
+    sum,
+    sub,
+    mult,
+    div,
+}
---
+++ index.js
@@ -0,0 +1,8 @@
+const calc = require('./calculator');
+
+console.log(calc.sum(1,1));
+console.log(calc.sum(-123,123123));
+console.log(calc.mult(8,0));
+console.log(calc.mult(1239123,12313));
+console.log(calc.div(123,-12));
+console.log(calc.div(313123,0));
